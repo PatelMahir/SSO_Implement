f727283d04ef5f4d6d579dc5e6d46a70ae554cd5
PatelMahir <87660306+PatelMahir@users.noreply.github.com>
2024-10-03 12:39:27 +0530

    Added Get API in the syntax with controller action.

---
+++ SSO_Implement/Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace SSO_Implement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet("public-data")]
+        public IActionResult GetPublicData()
+        {
+            var publicData = new { Message = "This is public data accessible without authentication " };
+            return Ok(publicData);
+        }
+        [Authorize]
+        [HttpGet("protected-data")]
+        public IActionResult GetProtectedData()
+        {
+            var protectedData = new { Message = "This is protected data accessible only with authentication" };
+            return Ok(protectedData);
+        }
+    }
+}
